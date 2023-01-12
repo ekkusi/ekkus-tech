16b78ca0d7603ab2778919b8059712c8b9b43c43
Ekku Sipilä <ekku.eki@gmail.com>
2023-01-12 16:56:09 +0200

    For real fix font preloading

--- site/gatsby-browser.tsx
+++ site/gatsby-browser.tsx
@@ -42,7 +42,7 @@ export const wrapPageElement = ({ element }: WrapPageElementNodeArgs) => {
             font-style: normal;
             font-weight: 700;
             font-display: swap;
-            src: url("/fonts/whatever-it-takes-bold.otf") format("truetype");
+            src: url("/fonts/whatever-it-takes-bold.ttf") format("truetype");
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
           }
           /* latin */
